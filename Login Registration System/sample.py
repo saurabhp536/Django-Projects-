92760624bdf959d6550e5fdeebaf3fed2029ad97
Saurabh Patil <100773962+saurabhp536@users.noreply.github.com>
2023-10-14 00:31:09 +0530

    sucessfully uploaded

--- Login Registration System/sample.py
+++
@@ -1,8 +0,0 @@
-import random
-import string
-
-def generate_secret_key(length=50):
-    characters = string.ascii_letters + string.digits + string.punctuation
-    return ''.join(random.choice(characters) for _ in range(length))
-
-print(generate_secret_key())
